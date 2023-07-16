dcb6578e81eb9f3cc6edf4f6734ce91b47c7a967
Rohitsingh0103 <singhrohitrs4@gmail.com>
2023-07-16 06:08:55 -0700

    Uploading Initial Automation Framework New

--- src/test/java/com/tutorialsninja/automation/runner/Runner.java
+++ src/test/java/com/tutorialsninja/automation/runner/Runner.java
@@ -1,5 +1,19 @@
 package com.tutorialsninja.automation.runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"classpath:FeatureFiles/Register.feature" },
+glue={"classpath:com.tutorialsninja.automation.stepdef"},
+plugin={"html:target/cucumber_html_reports"})
 public class Runner {
 
 }
